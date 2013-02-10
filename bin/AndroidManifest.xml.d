bb2478d59ea1528f88dd0bef0b938cb8227683ab
damienChesneau <ches.damien@gmail.com>
2013-02-10 17:24:22 +0100

    first commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/damien/NetBeansProjects/securisation-site-gvi-android/bin/AndroidManifest.xml : \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/AndroidManifest.xml \
---
+++ gen/R.java.d
@@ -0,0 +1,10 @@
+/home/damien/NetBeansProjects/securisation-site-gvi-android/gen/fr/securisation_site_gvi/R.java \
+ : /home/damien/NetBeansProjects/securisation-site-gvi-android/res/drawable-ldpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/res/layout/main.xml \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/res/drawable-hdpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/res/values/strings.xml \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/res/drawable-mdpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/bin/res/drawable-ldpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/bin/res/drawable-hdpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/bin/res/drawable-mdpi/ic_launcher.png \
+/home/damien/NetBeansProjects/securisation-site-gvi-android/bin/AndroidManifest.xml \
