8ca81598f1e141f3b9a4c643579a73581a80a645
damienChesneau <ches.damien@gmail.com>
2013-02-14 14:16:09 +0100

    add tier for serviceweb

---
+++ src/ressources/Ressources.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ressources;
+
+/**
+ *
+ * @author damien
+ */
+public class Ressources {
+    private final static String protocol ="http://";
+    private final static String serveurURL =/*"192.168.1.11";"localhost"*/"172.16.79.118";
+    private final static int  port = 8080;
+    private final static String applicationName ="securisation-site-gvi-web";
+    private final static String resourcesPath ="webresources";
+    private final static String pathToAccesWebService =protocol+serveurURL+":"+port+"/"+applicationName+"/"+resourcesPath+"/";
+
+    public static String getProtocol() {
+        return protocol;
+    }
+
+    public static String getServeurURL() {
+        return serveurURL;
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+    public static String getApplicationName() {
+        return applicationName;
+    }
+
+    public static String getResourcesPath() {
+        return resourcesPath;
+    }
+
+    public static String getPathToAccesWebService() {
+        return pathToAccesWebService;
+    }
+}
