95718a324adea80181d6685b13303d26b43d6c2e
damienChesneau <ches.damien@gmail.com>
2013-02-16 10:07:31 +0100

    update progressDialog for dismit when the thread have finish

--- src/fr/securisation_site_gvi/client/MainActivity.java
+++ src/fr/securisation_site_gvi/client/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +28,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         this.buttonConnnexion = (Button) findViewById(R.id.buttonConnexion);
         this.editTextLogin = (EditText) findViewById(R.id.editTextLogin);
         this.textView = (TextView) findViewById(R.id.textView1);
@@ -36,12 +39,13 @@ public class MainActivity extends Activity {
                 t.setLogin(editTextLogin.getText().toString());
                 t.setPassword(editTextPassword.getText().toString());
                 Technicien tech = null;
-                 ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, "", "Verification...", true);
+                 ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, "", "Vérification ...", true);
                 try {
                     tech = utilisateurSrv.verificationConnexion(t);
                 } catch (Exception ex) {
                     Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
                 }
+                progressDialog.dismiss();
                 if (tech == null) {
                     textView.setText("Connexion erreur.");
                    Toast to= Toast.makeText(MainActivity.this,"Il y à une erreur dans votre login ou votre mot de passe.",Toast.LENGTH_LONG);
@@ -50,6 +54,7 @@ public class MainActivity extends Activity {
                     textView.setText("Connexion réussi.");
                     
                     Intent intent = new Intent(MainActivity.this, AccueilActivity.class);
+
                     startActivity(intent);
                 }
             }
@@ -58,4 +63,21 @@ public class MainActivity extends Activity {
 
 
     }
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			// Comportement du bouton "A Propos"
+			return true;
+		
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
