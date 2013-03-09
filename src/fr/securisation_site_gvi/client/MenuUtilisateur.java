d81dc38f023480e150981301083fc9ad7fd60925
damienChesneau <ches.damien@gmail.com>
2013-03-09 10:35:34 +0100

    add Adroid for MenuUtilisateur

---
+++ src/fr/securisation_site_gvi/client/MenuUtilisateur.java
@@ -0,0 +1,22 @@
+package fr.securisation_site_gvi.client;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MenuUtilisateur extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_utilisateur);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_accueil, menu);
+		return true;
+	}
+
+}
