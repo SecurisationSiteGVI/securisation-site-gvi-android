b44db04625b1814b8907c784fd2204a813796168
damienChesneau <ches.damien@gmail.com>
2013-03-12 10:57:49 +0100

    update location of webservice

---
+++ src/physique/dataOut/PhysiqueDataOutFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.dataOut;
+
+import java.util.List;
+
+/**
+ *
+ * @author damien
+ */
+public class PhysiqueDataOutFactory {
+
+    private static UtilisateurServiceWeb personneClientSrv = new UtilisateurServiceWebImpl();
+    
+    public static UtilisateurServiceWeb getPersonneClientServiceWeb(){
+        return personneClientSrv;
+    }
+}
---
+++ src/physique/dataOut/UtilisateurServiceWeb.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.dataOut;
+
+
+import metier.entitys.Technicien;
+import metier.entitys.Utilisateur;
+import java.util.List;
+
+/**
+ *
+ * @author damien
+ */
+public interface UtilisateurServiceWeb {
+    public List<Utilisateur> getAll() throws Exception;
+    public List<Utilisateur> getAll(int from,int nbResut) throws Exception;
+    public boolean add(Utilisateur utilisateur) throws Exception;
+    public boolean remove(Utilisateur utilisateur) throws Exception;
+    public boolean update(Utilisateur utilisateur) throws Exception;
+    public boolean loginIsUse(String login) throws Exception;
+    public Technicien verificationConnexion(Technicien utilisateur) throws Exception;
+    
+}
