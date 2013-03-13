22fce50392fcaad96e2c8b658c1f67599d5f3f44
damienChesneau <ches.damien@gmail.com>
2013-03-13 11:44:39 +0100

    compil

--- bin/securisation-site-gvi-android.ap_.d
+++ bin/securisation-site-gvi-android.ap_.d
@@ -6,8 +6,10 @@
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/drawable-xhdpi/ic_action_help.png \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/drawable-xhdpi/ic_action_settings.png \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/main.xml \
+/home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/modifier_utilisateur.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/accueilactivity.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/activity_menu_utilisateur.xml \
+/home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/activity_parametres.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/layout/activity_liste_utilisateur.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/drawable-hdpi/ic_action_about.png \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/drawable-hdpi/ic_action_search_1.png \
@@ -36,6 +38,8 @@
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/values/dimens.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/values/styles.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/drawable-ldpi/ic_launcher.png \
+/home/damien/ProjetBTS/securisation-site-gvi-android/res/values-sw720dp-land/dimens.xml \
+/home/damien/ProjetBTS/securisation-site-gvi-android/res/values-sw600dp/dimens.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/values-v11/styles.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/res/values-large/dimens.xml \
 /home/damien/ProjetBTS/securisation-site-gvi-android/bin/res/drawable-xhdpi/ic_action_about.png \
