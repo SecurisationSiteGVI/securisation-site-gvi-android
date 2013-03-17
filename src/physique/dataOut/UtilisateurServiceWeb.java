f02187d7910d32400772541e98331ab9ff31929e
damienChesneau <ches.damien@gmail.com>
2013-03-17 12:29:10 +0100

    add context

--- src/physique/dataOut/UtilisateurServiceWeb.java
+++ src/physique/dataOut/UtilisateurServiceWeb.java
@@ -5,6 +5,7 @@
 package physique.dataOut;
 
 
+import android.content.Context;
 import metier.entitys.Technicien;
 import metier.entitys.Utilisateur;
 import java.util.List;
@@ -14,13 +15,13 @@ import java.util.List;
  * @author damien
  */
 public interface UtilisateurServiceWeb {
-    public List<Utilisateur> getAll() throws Exception;
-    public List<Utilisateur> getAll(int from,int nbResut) throws Exception;
-    public boolean add(Utilisateur utilisateur) throws Exception;
-    public boolean remove(Utilisateur utilisateur) throws Exception;
-    public boolean update(Utilisateur utilisateur) throws Exception;
-    public boolean loginIsUse(String login) throws Exception;
-    public Technicien verificationConnexion(Technicien utilisateur) throws Exception;
-    public Utilisateur getById(Long id)throws Exception;
+    public List<Utilisateur> getAll(Context context) throws Exception;
+    public List<Utilisateur> getAll(int from,int nbResut,Context context) throws Exception;
+    public boolean add(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean remove(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean update(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean loginIsUse(String login,Context context) throws Exception;
+    public Technicien verificationConnexion(Technicien utilisateur,Context context) throws Exception;
+    public Utilisateur getById(Long id,Context context)throws Exception;
     
 }
