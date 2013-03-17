3e366c5d334631dd45d57eaa99ad1847bc3034c7
damienChesneau <ches.damien@gmail.com>
2013-03-17 12:27:46 +0100

    impl of method into UtilisateurService

--- src/metier/UtilisateurServiceImpl.java
+++ src/metier/UtilisateurServiceImpl.java
@@ -4,6 +4,7 @@
  */
 package metier;
 
+import android.content.Context;
 import java.util.List;
 import metier.entitys.Technicien;
 import metier.entitys.Utilisateur;
@@ -16,19 +17,19 @@ import physique.dataOut.UtilisateurServiceWeb;
  */
 public class UtilisateurServiceImpl implements UtilisateurService{
     private UtilisateurServiceWeb utilisateurSrv = PhysiqueDataOutFactory.getPersonneClientServiceWeb();
-    public List<Utilisateur> getAll() throws Exception {
-        return this.utilisateurSrv.getAll();
+    public List<Utilisateur> getAll(Context context) throws Exception {
+        return this.utilisateurSrv.getAll(context);
     }
 
-    public List<Utilisateur> getAll(int from, int nbResut) throws Exception {
-        return this.utilisateurSrv.getAll(from, nbResut);
+    public List<Utilisateur> getAll(int from, int nbResut,Context context) throws Exception {
+        return this.utilisateurSrv.getAll(from, nbResut,context);
     }
 
-    public boolean add(Utilisateur utilisateur) throws Exception {
+    public boolean add(Utilisateur utilisateur,Context context) throws Exception {
         Boolean b = false;
         if (utilisateur != null) {
             if (utilisateur instanceof Utilisateur) {
-                b=utilisateurSrv.add(utilisateur);
+                b=utilisateurSrv.add(utilisateur,context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -37,11 +38,11 @@ public class UtilisateurServiceImpl implements UtilisateurService{
         }return b;
     }
 
-    public boolean remove(Utilisateur utilisateur) throws Exception {
+    public boolean remove(Utilisateur utilisateur,Context context) throws Exception {
         Boolean b = false;
         if (utilisateur != null) {
             if (utilisateur instanceof Utilisateur) {
-                b=utilisateurSrv.add(utilisateur);
+                b=utilisateurSrv.remove(utilisateur,context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -50,11 +51,11 @@ public class UtilisateurServiceImpl implements UtilisateurService{
         }return b;
     }
 
-    public boolean update(Utilisateur utilisateur) throws Exception {
+    public boolean update(Utilisateur utilisateur,Context context) throws Exception {
         Boolean b = false;
         if (utilisateur != null) {
             if (utilisateur instanceof Utilisateur) {
-                b=utilisateurSrv.update(utilisateur);
+                b=utilisateurSrv.update(utilisateur,context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -63,11 +64,11 @@ public class UtilisateurServiceImpl implements UtilisateurService{
         }return b;
     }
 
-    public boolean loginIsUse(String login) throws Exception {
+    public boolean loginIsUse(String login,Context context) throws Exception {
         Boolean b = false;
         if (login != null) {
             if (login instanceof String) {
-               b= utilisateurSrv.loginIsUse(login);
+               b= utilisateurSrv.loginIsUse(login,context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -76,11 +77,11 @@ public class UtilisateurServiceImpl implements UtilisateurService{
         }return b;
     }
 
-    public Technicien verificationConnexion(Technicien utilisateur) throws Exception {
+    public Technicien verificationConnexion(Technicien utilisateur,Context context) throws Exception {
         Technicien  technicien = null;
         if (utilisateur != null) {
             if (utilisateur instanceof Utilisateur) {
-                technicien = utilisateurSrv.verificationConnexion(utilisateur);
+                technicien = utilisateurSrv.verificationConnexion(utilisateur, context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -89,11 +90,11 @@ public class UtilisateurServiceImpl implements UtilisateurService{
         }return technicien;
     }
 
-    public Utilisateur getById(Long id) throws Exception {
+    public Utilisateur getById(Long id,Context context) throws Exception {
         Utilisateur u = null;
         if (id != null) {
             if (id instanceof Long) {
-                u=utilisateurSrv.getById(id);
+                u=utilisateurSrv.getById(id,context);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
