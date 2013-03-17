de9520cd4705fd72038c9ad6241316cc84c2823c
damienChesneau <ches.damien@gmail.com>
2013-03-17 12:27:26 +0100

    add method into UtilisateurService

--- src/metier/UtilisateurService.java
+++ src/metier/UtilisateurService.java
@@ -4,6 +4,7 @@
  */
 package metier;
 
+import android.content.Context;
 import java.util.List;
 import metier.entitys.Technicien;
 import metier.entitys.Utilisateur;
@@ -13,12 +14,12 @@ import metier.entitys.Utilisateur;
  * @author damien
  */
 public interface UtilisateurService {
-    public List<Utilisateur> getAll() throws Exception;
-    public List<Utilisateur> getAll(int from,int nbResut) throws Exception;
-    public boolean add(Utilisateur utilisateur) throws Exception;
-    public boolean remove(Utilisateur utilisateur) throws Exception;
-    public boolean update(Utilisateur utilisateur) throws Exception;
-    public boolean loginIsUse(String login) throws Exception;
-    public Technicien verificationConnexion(Technicien utilisateur) throws Exception;
-    public Utilisateur getById(Long id)throws Exception;
+    public List<Utilisateur> getAll(Context context) throws Exception;
+    public List<Utilisateur> getAll(int from,int nbResut,Context context) throws Exception;
+    public boolean add(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean remove(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean update(Utilisateur utilisateur,Context context) throws Exception;
+    public boolean loginIsUse(String login,Context context) throws Exception;
+    public Technicien verificationConnexion(Technicien utilisateur,Context context) throws Exception;
+    public Utilisateur getById(Long id,Context context)throws Exception;
 }
