2ac241e463a92bd7afbc6f0ad8a10a0d231696ce
damienChesneau <ches.damien@gmail.com>
2013-03-17 12:28:52 +0100

    impl for SQlite database

--- src/physique/dataIn/RessourceServiceDataInServiceImpl.java
+++ src/physique/dataIn/RessourceServiceDataInServiceImpl.java
@@ -16,7 +16,7 @@ import metier.entitys.Ressource;
  *
  * @author damien
  */
-public class RessourceServiceDataInServiceImpl implements RessourcesServiceDataIn{
+public class RessourceServiceDataInServiceImpl implements RessourcesServiceDataIn {
 
     public static final String RESSOURCE_KEY = "id";
     public static final String RESSOURCE_PROTOCOL = "protocol";
@@ -26,18 +26,19 @@ public class RessourceServiceDataInServiceImpl implements RessourcesServiceDataI
     public static final String RESSOURCE_RESSOURCES_PATH = "ressourcesPath";
     public static final String RESSOURCE_TABLE_NAME = "Ressource";
     private Connexion connexion;
-    private SQLiteDatabase db ;
+    private SQLiteDatabase db;
     private Context c;
+
     public RessourceServiceDataInServiceImpl(Context c) {
         this.c = c;
     }
 
     public void add(Ressource ressource) throws Exception {
         this.connexion = new Connexion(c, "fr.db", null, Connexion.VERSION);
-        db = connexion.mDb;
+        db = connexion.getBDD();
         this.connexion.open();
         ContentValues values = new ContentValues();
-        values.put(RESSOURCE_KEY, 1);
+          values.put(RESSOURCE_KEY, 1);
         values.put(RESSOURCE_PROTOCOL, ressource.getProtocol());
         values.put(RESSOURCE_SERVEUR_URL, ressource.getServeurURL());
         values.put(RESSOURCE_PORT, ressource.getPort());
@@ -50,21 +51,29 @@ public class RessourceServiceDataInServiceImpl implements RessourcesServiceDataI
     public Ressource getRessource() throws Exception {
         this.connexion = new Connexion(c, "fr.db", null, Connexion.VERSION);
         this.connexion.open();
-        Cursor c = this.connexion.mDb.query(RESSOURCE_TABLE_NAME, new String[]{RESSOURCE_KEY, RESSOURCE_APPLICATION_NAME,
-                    RESSOURCE_PORT, RESSOURCE_PROTOCOL, RESSOURCE_RESSOURCES_PATH, RESSOURCE_SERVEUR_URL}, 1 + "", null, null, null, null);
-        this.connexion.close();
+        Cursor c = this.connexion.mDb.rawQuery("select * from " + RESSOURCE_TABLE_NAME + " where id = 1", null);
+//        Cursor c = this.connexion.mDb.query(RESSOURCE_TABLE_NAME, new String[]{RESSOURCE_KEY, RESSOURCE_APPLICATION_NAME,
+//                    RESSOURCE_PORT, RESSOURCE_PROTOCOL, RESSOURCE_RESSOURCES_PATH, RESSOURCE_SERVEUR_URL}, null, null, null, null, null);
+//        
         Ressource ressource = new Ressource();
-        ressource.setProtocol(c.getString(c.getColumnIndex(RESSOURCE_PROTOCOL)));
-        ressource.setPort(c.getInt(c.getColumnIndex(RESSOURCE_PORT)));
-        ressource.setServeurURL(c.getString(c.getColumnIndex(RESSOURCE_SERVEUR_URL)));
-        ressource.setResourcesPath(c.getString(c.getColumnIndex(RESSOURCE_RESSOURCES_PATH)));
-        ressource.setApplicationName(c.getString(c.getColumnIndex(RESSOURCE_APPLICATION_NAME)));
+//        if (c.getCount()>0) {
+            c.moveToNext();
+            int columnIndex = c.getColumnIndex(RESSOURCE_PROTOCOL);
+            String string = c.getString(columnIndex);
+            ressource.setProtocol(string);
+            ressource.setPort(c.getInt(c.getColumnIndex(RESSOURCE_PORT)));
+            ressource.setServeurURL(c.getString(c.getColumnIndex(RESSOURCE_SERVEUR_URL)));
+            ressource.setResourcesPath(c.getString(c.getColumnIndex(RESSOURCE_RESSOURCES_PATH)));
+            ressource.setApplicationName(c.getString(c.getColumnIndex(RESSOURCE_APPLICATION_NAME)));
+//        }
+        this.connexion.close();
         return ressource;
     }
 
     public void update(Ressource ressource) throws Exception {
         this.connexion = new Connexion(c, "fr.db", null, Connexion.VERSION);
         connexion.open();
+       
         ContentValues values = new ContentValues();
         values.put(RESSOURCE_KEY, 1);
         values.put(RESSOURCE_PROTOCOL, ressource.getProtocol());
