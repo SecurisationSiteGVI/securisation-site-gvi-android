12eb09d3220787872afcf4fa492d3e93aa0abfa9
damienChesneau <ches.damien@gmail.com>
2013-03-23 08:58:00 +0100

    Add EvenementServiceImpl

---
+++ src/metier/EvenementServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import android.content.Context;
+import java.util.List;
+import metier.entitys.Evenement;
+
+/**
+ *
+ * @author damien
+ */
+public class EvenementServiceImpl implements EvenementService{
+
+    public List<Evenement> getAll(Context context) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Evenement> getAll(Context context, int index, int nbResultat) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
