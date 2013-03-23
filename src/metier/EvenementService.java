d8dd4c944165465d42ebe24eaefdf900a69e3d5b
damienChesneau <ches.damien@gmail.com>
2013-03-23 08:57:53 +0100

    Add EvenementService

---
+++ src/metier/EvenementService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import android.content.Context;
+import java.util.List;
+import metier.entitys.Evenement;
+
+/**
+ *
+ * @author damien
+ */
+public interface EvenementService {
+    public List<Evenement> getAll(Context context) throws Exception;
+    public List<Evenement> getAll(Context context,int index, int nbResultat) throws Exception;
+}
