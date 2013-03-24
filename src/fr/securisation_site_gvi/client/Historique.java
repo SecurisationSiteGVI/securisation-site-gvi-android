21560367c27aa09818ce7a29b7a0a16ac32f886b
damienChesneau <ches.damien@gmail.com>
2013-03-24 17:47:12 +0100

    add pagination

--- src/fr/securisation_site_gvi/client/Historique.java
+++ src/fr/securisation_site_gvi/client/Historique.java
@@ -1,17 +1,155 @@
 package fr.securisation_site_gvi.client;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import metier.EvenementService;
+import metier.MetierFactory;
+import metier.entitys.Evenement;
+import metier.entitys.Utilisateur;
+import physique.dataOut.PhysiqueDataOutFactory;
+import physique.dataOut.UtilisateurServiceWeb;
 
 public class Historique extends TemplateActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_historique);
-         
-	}
+    private ListView listUtilisateurs;
+    private Button precedent;
+    private Button suivant;
+    private EvenementService utilisateurSrv = MetierFactory.getEvenementSrv();
+    private int index;
+    private int nbLinge = 10;
+    private TextView textViewPage;
+    private List<Evenement> u;
+    private int pos;
+    private int count;
 
-	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historique);
+        this.setThisActivityOn();
+        this.index = 0;
+        if (this.isTablette7()) {
+            this.nbLinge = 15;
+        }
+        try {
+            this.count = this.utilisateurSrv.count(this.activityContext);
+        } catch (Exception ex) {
+            Logger.getLogger(ListeUtilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.remplirListView();
+    }
 
+    private void remplirListView() {
+        if (index == 0) {
+            this.precedent.setEnabled(false);
+        } else {
+            this.precedent.setEnabled(true);
+        }
+        if (this.getPage() + 1 == this.getNbPages()+1) {
+            this.suivant.setEnabled(false);
+        } else {
+            this.suivant.setEnabled(true);
+        }
+        List<Evenement> utilisateurs = null;
+        try {
+            utilisateurs = this.utilisateurSrv.getAll(activityContext);
+            this.u = utilisateurs;
+        } catch (Exception ex) {
+            Logger.getLogger(ListeUtilisateur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String[] listeStrings = new String[utilisateurs.size()];
+        for (int i = 0; i < utilisateurs.size(); i++) {
+            listeStrings[i] = utilisateurs.get(i).toString();
+        }
+        listUtilisateurs.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listeStrings));
+        this.textViewPage.setText("Page " + this.getPage() + "/" + this.getNbPages());
+    }
+
+    public void pagePrécédente() {
+        if (this.index <= nbLinge - 1) {
+            Toast.makeText(activityContext, "Vous éte déjà sur la premiere page.", Toast.LENGTH_SHORT).show();
+        } else {
+            this.index = this.index - nbLinge;
+            this.remplirListView();
+        }
+    }
+
+    public void pageSuivante() {
+        if(this.getPage()>= this.getNbPages()){
+            Toast.makeText(this.activityContext, "Vous étes déjà sur la derniére page", Toast.LENGTH_LONG).show();
+            this.suivant.setEnabled(false);
+        }
+        this.index = this.index + nbLinge;
+        this.remplirListView();
+    }
+
+    public int getPage() {
+        int page = index / nbLinge;
+        page = page + 1;
+        return page;
+
+    }
+
+    private int getNbPages() {
+        int nbPages = this.count / this.nbLinge;
+        return nbPages+1;
+    }
+
+    @Override
+    public void initGraphicalObjects() {
+        this.listUtilisateurs = (ListView) findViewById(R.id.HistoriquelisteEvenements);
+        this.precedent = (Button) findViewById(R.id.HistoriqueboutonPrecedent);
+        this.suivant = (Button) findViewById(R.id.HistoriqueboutonSuivant);
+        this.textViewPage = (TextView) findViewById(R.id.HistoriquetextPage);
+    }
+
+    @Override
+    public void addActionListnerForAllGraphicalObjects() {
+        this.listUtilisateurs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                pos = position;
+                AlertDialog.Builder builder = new AlertDialog.Builder(activityContext);
+                builder.setTitle("Utilisateur séléctionné.");
+                builder.setMessage(u.get(position).toString() + " à été séléctionné voulez vous le modifier ?");
+                builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i = new Intent(activityContext, ModifierUtilisateur.class);
+                        i.putExtra("id", u.get(pos).getId());
+                        startActivityForResult(i, 0);
+                        dialog.cancel();
+                    }
+                });
+                builder.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+        this.precedent.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pagePrécédente();
+            }
+        });
+        this.suivant.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pageSuivante();
+            }
+        });
+    }
 }
