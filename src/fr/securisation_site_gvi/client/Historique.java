4839366561d361c991f03ea882920cd1d81101b8
damienChesneau <ches.damien@gmail.com>
2013-03-27 18:31:35 +0100

    update pagination

--- src/fr/securisation_site_gvi/client/Historique.java
+++ src/fr/securisation_site_gvi/client/Historique.java
@@ -2,6 +2,7 @@ package fr.securisation_site_gvi.client;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,7 +16,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import metier.EvenementService;
 import metier.MetierFactory;
+import metier.entitys.Acces;
 import metier.entitys.Evenement;
+import metier.entitys.Intrusion;
+import metier.entitys.Photo;
 
 public class Historique extends TemplateActivity {
 
@@ -24,7 +28,7 @@ public class Historique extends TemplateActivity {
     private Button suivant;
     private EvenementService evenementSrv = MetierFactory.getEvenementSrv();
     private int index;
-    private int nbLinge = 10;
+    private int nbLinge = 9;
     private TextView textViewPage;
     private List<Evenement> u;
     private int pos;
@@ -53,7 +57,7 @@ public class Historique extends TemplateActivity {
         } else {
             this.precedent.setEnabled(true);
         }
-        if (this.getPage() + 1 == this.getNbPages()+1) {
+        if (this.getPage() + 1 == this.getNbPages() + 1) {
             this.suivant.setEnabled(false);
         } else {
             this.suivant.setEnabled(true);
@@ -83,7 +87,7 @@ public class Historique extends TemplateActivity {
     }
 
     public void pageSuivante() {
-        if(this.getPage()>= this.getNbPages()){
+        if (this.getPage() >= this.getNbPages()) {
             Toast.makeText(this.activityContext, "Vous étes déjà sur la derniére page", Toast.LENGTH_LONG).show();
             this.suivant.setEnabled(false);
         }
@@ -100,7 +104,7 @@ public class Historique extends TemplateActivity {
 
     private int getNbPages() {
         int nbPages = this.count / this.nbLinge;
-        return nbPages+1;
+        return nbPages + 1;
     }
 
     @Override
@@ -123,9 +127,23 @@ public class Historique extends TemplateActivity {
                 builder.setMessage("Vous avez séléctionné un évement voulez vous avoir des détails ?");
                 builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-//                        Intent i = new Intent(activityContext, ModifierUtilisateur.class);
-//                        i.putExtra("id", u.get(pos).getId());
-//                        startActivityForResult(i, 0);
+                        Long idCliked = u.get(pos).getId();
+                        Evenement evenement = null;
+                        try {
+                            evenement = evenementSrv.getById(activityContext, idCliked);
+                        } catch (Exception ex) {
+                            Logger.getLogger(Historique.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        if (evenement instanceof Acces) {
+                            Intent i = new Intent(activityContext, HistoriqueAcces.class);
+                            i.putExtra("id", u.get(pos).getId());
+                            startActivityForResult(i, 0);
+                        } else if (evenement instanceof Photo) {
+                            Intent i = new Intent(activityContext, HistoriquePhoto.class);
+                            i.putExtra("id", u.get(pos).getId());
+                            startActivityForResult(i, 0);
+                        } else if (evenement instanceof Intrusion) {
+                        }
                         dialog.cancel();
                     }
                 });
