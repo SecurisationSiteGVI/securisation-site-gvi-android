b1dbc308edc79ff4045a04b33537808b5bbb32d0
damienChesneau <ches.damien@gmail.com>
2013-03-28 14:39:34 +0100

    add activity

--- bin/securisation-site-gvi-android-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/damien/ProjetBTS/securisation-site-gvi-android/bin/securisation-site-gvi-android-debug-unaligned.apk : \
-/home/damien/ProjetBTS/securisation-site-gvi-android/bin/securisation-site-gvi-android.ap_ \
-/home/damien/ProjetBTS/securisation-site-gvi-android/bin/classes.dex \
-/home/damien/android-sdks/tools/support/annotations.jar \
-/home/damien/ProjetBTS/securisation-site-gvi-android/libs/android-support-v4.jar \
