d9ac86f9ae4148d51c5d106f3e959428388f3a82
damienChesneau <ches.damien@gmail.com>
2013-03-29 11:15:51 +0100

    add BadgeServiceWeb interface

---
+++ src/physique/dataOut/badge/BadgeServiceWeb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.dataOut.badge;
+
+import android.content.Context;
+import java.util.List;
+import metier.entitys.Badge;
+import metier.entitys.Ressource;
+
+
+/**
+ *
+ * @author damien
+ */
+public interface BadgeServiceWeb {
+    public List<Badge> getAll(Context context,Ressource ressource) throws Exception;
+    public List<Badge> getAll(Context context,Ressource ressource,int index, int nbResultat) throws Exception;
+    public int count(Context c,Ressource ressource) throws Exception;
+}
