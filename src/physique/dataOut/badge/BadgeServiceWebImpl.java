498024cfcb31988877fe2589e9336a42aca0f25b
damienChesneau <ches.damien@gmail.com>
2013-03-31 10:58:05 +0200

    add RESTBadgeCount asynctask

--- src/physique/dataOut/badge/BadgeServiceWebImpl.java
+++ src/physique/dataOut/badge/BadgeServiceWebImpl.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import java.util.List;
 import metier.entitys.Badge;
 import metier.entitys.Ressource;
+import physique.dataOut.badge.asyncTask.RESTBadgeCount;
 import physique.dataOut.badge.asyncTask.RESTBadgeGetAll;
 import physique.dataOut.badge.asyncTask.RESTBadgeGetAllByRange;
 
@@ -16,23 +17,23 @@ import physique.dataOut.badge.asyncTask.RESTBadgeGetAllByRange;
  *
  * @author damien
  */
-public class BadgeServiceWebImpl implements BadgeServiceWeb{
+public class BadgeServiceWebImpl implements BadgeServiceWeb {
 
     public List<Badge> getAll(Context context, Ressource ressource) throws Exception {
-        AsyncTask<Object, Void, Object> ret = new RESTBadgeGetAll().execute(context,ressource);
+        AsyncTask<Object, Void, Object> ret = new RESTBadgeGetAll().execute(context, ressource);
         List<Badge> retour = (List<Badge>) ret.get();
         return retour;
     }
 
     public List<Badge> getAll(Context context, Ressource ressource, int index, int nbResultat) throws Exception {
-        AsyncTask<Object, Void, Object> ret = new RESTBadgeGetAllByRange().execute(context,ressource,index,nbResultat);
+        AsyncTask<Object, Void, Object> ret = new RESTBadgeGetAllByRange().execute(context, ressource, index, nbResultat);
         List<Badge> retour = (List<Badge>) ret.get();
         return retour;
     }
 
-    public int count(Context c, Ressource ressource) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int count(Context context, Ressource ressource) throws Exception {
+        AsyncTask<Object, Void, Object> ret = new RESTBadgeCount().execute(context, ressource);
+        Integer retour = (Integer) ret.get();
+        return retour;
     }
-
-    
 }
