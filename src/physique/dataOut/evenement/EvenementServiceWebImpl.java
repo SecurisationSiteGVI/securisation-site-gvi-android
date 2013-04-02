bfd0b1d70495efb8fcdf929897e3a8ab58dc3761
damienChesneau <ches.damien@gmail.com>
2013-04-02 15:01:36 +0200

    Move code into external class

---
+++ src/physique/dataOut/evenement/EvenementServiceWebImpl.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.dataOut.evenement;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import java.util.List;
+import metier.entitys.Evenement;
+import physique.dataOut.evenement.asynctask.RESTEvenementCount;
+import physique.dataOut.evenement.asynctask.RESTEvenementGetAll;
+import physique.dataOut.evenement.asynctask.RESTEvenementGetAllByRange;
+import physique.dataOut.evenement.asynctask.RESTEvenementGetByID;
+
+/**
+ *
+ * @author damien
+ */
+public class EvenementServiceWebImpl implements EvenementServiceWeb {
+
+    public List<Evenement> getAll(Context context) throws Exception {
+        AsyncTask<Object, Void, Object> ret = new RESTEvenementGetAll().execute(context);
+        List<Evenement> retour = (List<Evenement>) ret.get();
+        return retour;
+    }
+
+    public List<Evenement> getAll(Context context, int index, int nbResultat) throws Exception {
+        AsyncTask<Object, Void, Object> ret = new RESTEvenementGetAllByRange().execute(context, index, nbResultat);
+        List<Evenement> retour = (List<Evenement>) ret.get();
+        return retour;
+    }
+
+    public int count(Context c) throws Exception {
+        AsyncTask<Object, Void, Object> ret = new RESTEvenementCount().execute(c);
+        Integer count = (Integer) ret.get();
+        return count;
+    }
+
+    public Evenement getById(Context context, Long id) throws Exception {
+        AsyncTask<Object, Void, Object> ret = new RESTEvenementGetByID().execute(context, id);
+        Evenement retour = (Evenement) ret.get();
+        return retour;
+    }
+
+}
