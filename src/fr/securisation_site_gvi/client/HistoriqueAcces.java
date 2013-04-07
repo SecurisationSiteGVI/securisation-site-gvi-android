05cdf5b3ab619be68719124a00aa5678307438b9
damienChesneau <ches.damien@gmail.com>
2013-04-07 16:21:04 +0200

    add asynctask and throw all Exceptions

--- src/fr/securisation_site_gvi/client/HistoriqueAcces.java
+++ src/fr/securisation_site_gvi/client/HistoriqueAcces.java
@@ -4,15 +4,23 @@
  */
 package fr.securisation_site_gvi.client;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 import metier.EvenementService;
 import metier.MetierFactory;
 import metier.entitys.Acces;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -35,13 +43,7 @@ public class HistoriqueAcces extends TemplateActivity {
         setContentView(R.layout.activity_historique_acces);
         Bundle extras = getIntent().getExtras();
         this.id = extras.getLong("id");
-        try {
-            this.acces = (Acces) this.evenementSrv.getById(this.activityContext, this.id);
-        } catch (Exception ex) {
-            Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        this.setThisActivityOn();
-
+        new RESTEvenementGetById().execute();
     }
 
     @Override
@@ -73,5 +75,71 @@ public class HistoriqueAcces extends TemplateActivity {
         }
         this.textViewBorneAcces.setText(this.acces.getBorneAcces().toString());
 
+    }
+
+    private class RESTEvenementGetById extends AsyncTask<Object, Void, Object> {
+
+        private ProgressDialog progressDialog;
+        private boolean erreur = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.progressDialog = ProgressDialog.show(activityContext, "", "Récupération des informations ...", true);
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            this.progressDialog.cancel();
+            Boolean ret = (Boolean) result;
+            if (!erreur) {
+                acces = (Acces) result;
+                setThisActivityOn();
+            } else if (result instanceof MalformedURLException) {
+                throwMalformedURLException();
+            } else if (result instanceof SAXException) {
+                throwSAXException();
+            } else if (result instanceof ParseException) {
+                throwParseException();
+            } else if (result instanceof ParserConfigurationException) {
+                throwParseException();
+            } else if (result instanceof IOException) {
+                throwIOException();
+            } else {
+                throwException();
+            }
+        }
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            Object ret = null;
+            try {
+                ret = evenementSrv.getById(activityContext, id);
+            } catch (MalformedURLException ex) {
+                erreur = true;
+                ret = new MalformedURLException();
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                erreur = true;
+                ret = new IOException();
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SAXException ex) {
+                erreur = true;
+                ret = new SAXException();
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                erreur = true;
+                ret = new ParseException(" ", 1);
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParserConfigurationException ex) {
+                erreur = true;
+                ret = new ParserConfigurationException();
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                erreur = true;
+                Logger.getLogger(HistoriqueAcces.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return ret;
+        }
     }
 }
