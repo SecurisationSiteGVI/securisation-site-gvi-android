c6364557b433e761a935b6792e1c09c0b085ad5d
damienChesneau <ches.damien@gmail.com>
2013-04-07 17:21:05 +0200

    add asynctask and throw all Exceptions

--- src/fr/securisation_site_gvi/client/AjouterBadge.java
+++ src/fr/securisation_site_gvi/client/AjouterBadge.java
@@ -4,11 +4,16 @@
  */
 package fr.securisation_site_gvi.client;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import metier.BadgeService;
@@ -45,12 +50,53 @@ public class AjouterBadge extends TemplateActivity {
                 String numero = edittextNumero.getText().toString();
                 Badge b = new Badge();
                 b.setNumero(numero);
-                try {
-                    badgeSrv.add(activityContext, b);
-                } catch (Exception ex) {
-                    Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                
             }
         });
+    }
+    private class RESTUtilisateurAjout extends AsyncTask<Object, Void, Object> {
+
+        private ProgressDialog progressDialog;
+        private boolean erreur = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.progressDialog = ProgressDialog.show(activityContext, "", "Envoie des données ...", true);
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            this.progressDialog.cancel();
+            if (!erreur) {
+                Toast.makeText(activityContext, "Badge bien ajouté.", Toast.LENGTH_LONG).show();
+            }else if(result instanceof IOException){
+                throwIOException();
+            }else if(result instanceof MalformedURLException){
+                throwMalformedURLException();
+            }else{
+              throwException();
+            }
+        }
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            Object ret = null;
+            try {
+                badgeSrv.add(activityContext, (Badge)params[0]);
+            } catch (MalformedURLException ex) {
+                erreur=true;
+                ret= new MalformedURLException();
+                Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                erreur=true;
+                ret= new IOException();
+                Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                erreur=true;
+                Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
+            }               
+            return ret;
+        }
     }
 }
