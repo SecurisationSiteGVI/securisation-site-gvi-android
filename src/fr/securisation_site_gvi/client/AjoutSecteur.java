6e18983b13d0672d9af4908e2edf4d39a33a9a74
damienChesneau <ches.damien@gmail.com>
2013-04-07 15:23:15 +0200

    catching all Exception

--- src/fr/securisation_site_gvi/client/AjoutSecteur.java
+++ src/fr/securisation_site_gvi/client/AjoutSecteur.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import metier.BadgeService;
@@ -55,9 +56,11 @@ public class AjoutSecteur extends TemplateActivity {
             }
         });
     }
+
     private class RESTConexion extends AsyncTask<Object, Void, Object> {
 
         private ProgressDialog progressDialog;
+        private boolean erreur = false;
 
         @Override
         protected void onPreExecute() {
@@ -68,22 +71,29 @@ public class AjoutSecteur extends TemplateActivity {
         @Override
         protected void onPostExecute(Object result) {
             this.progressDialog.cancel();
-            if(result==null){
+            if (!erreur) {
                 Toast.makeText(activityContext, "Le secteur n'a pas put être ajouté.", Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(activityContext, "Le secteur a bien été ajouté.", Toast.LENGTH_LONG).show();
-            }           
+            } else if(result instanceof IOException){
+                throwIOException();
+            }else {
+                throwException();
+            }
         }
 
         @Override
         protected Object doInBackground(Object... params) {
+            Object ret = null;
             Secteur secteur = (Secteur) params[0];
             try {
-                   secteurSrv.ajouter(activityContext, secteur);
-                } catch (Exception ex) {
-                    secteur=null;
-                    Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                ret = secteurSrv.ajouter(activityContext, secteur);
+            } catch (IOException ex) {
+                erreur = true;
+                ret = new IOException();
+                Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                erreur = true;
+                Logger.getLogger(AjouterBadge.class.getName()).log(Level.SEVERE, null, ex);
+            }
             return secteur;
         }
     }
