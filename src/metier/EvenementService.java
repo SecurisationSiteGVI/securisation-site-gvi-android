39ef462a2581dc18be412cc4102a455326e8d039
damienChesneau <ches.damien@gmail.com>
2013-04-07 15:46:06 +0200

    remove asynctask and throw exception

--- src/metier/EvenementService.java
+++ src/metier/EvenementService.java
@@ -5,8 +5,13 @@
 package metier;
 
 import android.content.Context;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
 import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
 import metier.entitys.Evenement;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -14,11 +19,11 @@ import metier.entitys.Evenement;
  */
 public interface EvenementService {
 
-    public List<Evenement> getAll(Context context) throws Exception;
+    public List<Evenement> getAll(Context context) throws ParserConfigurationException, SAXException, IOException, Exception;
 
-    public List<Evenement> getAll(Context context, int index, int nbResultat) throws Exception;
+    public List<Evenement> getAll(Context context, int index, int nbResultat) throws ParserConfigurationException, SAXException, IOException, Exception;
 
-    public int count(Context c) throws Exception;
+    public int count(Context c) throws  IOException, Exception;
 
-    public Evenement getById(Context context, Long id) throws Exception;
+    public Evenement getById(Context context, Long id) throws MalformedURLException, IOException, SAXException, ParseException, ParserConfigurationException, Exception;
 }
--- src/metier/EvenementServiceImpl.java
+++ src/metier/EvenementServiceImpl.java
@@ -5,8 +5,16 @@
 package metier;
 
 import android.content.Context;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
 import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
 import metier.entitys.Evenement;
+import metier.entitys.Ressource;
+import org.xml.sax.SAXException;
+import physique.dataIn.PhysiqueDataInFactory;
+import physique.dataIn.RessourcesServiceDataIn;
 import physique.dataOut.evenement.EvenementServiceWeb;
 import physique.dataOut.PhysiqueDataOutFactory;
 
@@ -18,11 +26,11 @@ public class EvenementServiceImpl implements EvenementService {
 
     private EvenementServiceWeb evenementSrv = PhysiqueDataOutFactory.getEvenementServiceWeb();
 
-    public List<Evenement> getAll(Context context) throws Exception {
+    public List<Evenement> getAll(Context context) throws ParserConfigurationException, SAXException, IOException, Exception  {
         List<Evenement> b = null;
         if (context != null) {
             if (context instanceof Context) {
-                b = evenementSrv.getAll(context);
+                b = evenementSrv.getAll(this.getRessource(context));
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -32,11 +40,11 @@ public class EvenementServiceImpl implements EvenementService {
         return b;
     }
 
-    public List<Evenement> getAll(Context context, int index, int nbResultat) throws Exception {
+    public List<Evenement> getAll(Context context, int index, int nbResultat) throws ParserConfigurationException, SAXException, IOException, Exception  {
         List<Evenement> b = null;
         if (context != null) {
             if (context instanceof Context) {
-                b = evenementSrv.getAll(context, index, nbResultat);
+                b = evenementSrv.getAll(this.getRessource(context), index, nbResultat);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -46,11 +54,11 @@ public class EvenementServiceImpl implements EvenementService {
         return b;
     }
 
-    public int count(Context context) throws Exception {
+    public int count(Context context) throws IOException, Exception  {
         int b = 0;
         if (context != null) {
             if (context instanceof Context) {
-                b = evenementSrv.count(context);
+                b = evenementSrv.count(this.getRessource(context));
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -60,11 +68,11 @@ public class EvenementServiceImpl implements EvenementService {
         return b;
     }
 
-    public Evenement getById(Context context, Long id) throws Exception {
+    public Evenement getById(Context context, Long id) throws MalformedURLException, IOException, SAXException, ParseException, ParserConfigurationException, Exception  {
         Evenement b = null;
-        if (context != null) {
-            if (context instanceof Context) {
-                b = evenementSrv.getById(context, id);
+        if ((context != null)&&(id!=null)) {
+            if ((context instanceof Context)&&(id instanceof Long)) {
+                b = evenementSrv.getById(this.getRessource(context), id);
             } else {
                 System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
             }
@@ -72,5 +80,11 @@ public class EvenementServiceImpl implements EvenementService {
             throw new NullPointerException("Objet passé en parametre égale à null");
         }
         return b;
+    }
+    private Ressource getRessource(Context c) throws Exception {
+        RessourcesServiceDataIn ressourcesSrv = PhysiqueDataInFactory.getRessourceSrv(c);
+        Ressource ressource = null;
+        ressource = ressourcesSrv.getRessource();
+        return ressource;
     }
 }
