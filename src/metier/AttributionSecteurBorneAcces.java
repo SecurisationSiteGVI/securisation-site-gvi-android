9a13da1f6901fdab5eef909f699376c027a812fa
damienChesneau <ches.damien@gmail.com>
2013-04-09 12:12:48 +0200

    add attribuerSecteurBorneAccesServiceWeb

---
+++ src/metier/AttributionSecteurBorneAcces.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import android.content.Context;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import metier.entitys.BorneAcces;
+import metier.entitys.Ressource;
+import metier.entitys.Secteur;
+
+/**
+ *
+ * @author damien
+ */
+public interface AttributionSecteurBorneAcces {
+    public void attribuer(Context context, Secteur secteur, BorneAcces borneAcces)throws MalformedURLException, IOException, RuntimeException,Exception;
+    public void desattribuer(Context context, Secteur secteur, BorneAcces borneAcces)throws MalformedURLException, IOException, RuntimeException,Exception;
+}
