edcc95d459f68e33bdaf1c0cc9c578fb1d993979
damienChesneau <ches.damien@gmail.com>
2013-04-15 20:48:30 +0200

    update path

--- src/physique/dataOut/attributionSecteurBorneAcces/rest/RESTAttributionSecteurBorneAccesGetBySecteur.java
+++ src/physique/dataOut/attributionSecteurBorneAcces/rest/RESTAttributionSecteurBorneAccesGetBySecteur.java
@@ -4,36 +4,26 @@
  */
 package physique.dataOut.attributionSecteurBorneAcces.rest;
 
-import physique.dataOut.AttributionSecteurCamera.rest.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import metier.entitys.AttributionSecteurBorneAcces;
-import metier.entitys.AttributionSecteurCamera;
 import metier.entitys.BorneAcces;
-import metier.entitys.Camera;
 import metier.entitys.Position;
 import metier.entitys.Ressource;
 import metier.entitys.Secteur;
-import metier.entitys.Utilisateur;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import physique.dataOut.BoiteAOutils;
-import physique.dataOut.utilisateur.UtilisateurServiceWebImpl;
 
 /**
  *
