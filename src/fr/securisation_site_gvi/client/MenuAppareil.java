ff1c0b1c8a88b690cabb97f3fb5173438c8e5271
damienChesneau <ches.damien@gmail.com>
2013-04-20 11:48:51 +0200

    add menuDetecteur intrusion

--- src/fr/securisation_site_gvi/client/MenuAppareil.java
+++ src/fr/securisation_site_gvi/client/MenuAppareil.java
@@ -50,7 +50,8 @@ public class MenuAppareil extends TemplateActivity{
          this.buttonGererLesDetecteursIntrusion.setOnClickListener(new View.OnClickListener() {
 
              public void onClick(View v) {
-                 throw new UnsupportedOperationException("Not supported yet."); 
+                 Intent intent = new Intent(activityContext, MenuDetecteurIntrusion.class);
+                 startActivity(intent);
              }
          });
          this.buttonGererLesPositions.setOnClickListener(new View.OnClickListener() {
