0f5b19bbeb2962dd812355671aa3cb1b6907b306
damienChesneau <ches.damien@gmail.com>
2013-04-21 11:45:51 +0200

    add RESTDetecteurIntrusionAdd

---
+++ src/physique/dataOut/detecteurIntrusion/rest/RESTDetecteurIntrusionAdd.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.dataOut.detecteurIntrusion.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import metier.entitys.DetecteurIntrusion;
+import metier.entitys.Ressource;
+
+/**
+ *
+ * @author damien
+ */
+public class RESTDetecteurIntrusionAdd {
+    public static void execute(Ressource ressource, DetecteurIntrusion detecteurIntrusion) throws MalformedURLException, IOException  {
+        URL url = new URL(ressource.getPathToAccesWebService() + "detecteurintrusion/"+detecteurIntrusion.getPosition().getId()+"/"+detecteurIntrusion.getNom());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+            if (conn.getResponseCode() != 204) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            } else {
+                System.out.println("Requete envoyé mais pas de réponse du serveur.");
+            }
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+        conn.disconnect();
+    }
+}
