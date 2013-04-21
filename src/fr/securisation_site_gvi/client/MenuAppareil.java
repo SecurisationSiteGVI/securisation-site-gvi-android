eaefa02dab2d59e2a2a58ef914b064fec6181ffc
damienChesneau <ches.damien@gmail.com>
2013-04-21 18:53:50 +0200

    add link

--- src/fr/securisation_site_gvi/client/MenuAppareil.java
+++ src/fr/securisation_site_gvi/client/MenuAppareil.java
@@ -43,8 +43,8 @@ public class MenuAppareil extends TemplateActivity{
          this.buttonGererLesCameras.setOnClickListener(new View.OnClickListener() {
 
              public void onClick(View v) {
-                 throw new UnsupportedOperationException("Not supported yet."); 
-                 
+                 Intent intent = new Intent(activityContext, MenuCamera.class);
+                 startActivity(intent);
              }
          });
          this.buttonGererLesDetecteursIntrusion.setOnClickListener(new View.OnClickListener() {
