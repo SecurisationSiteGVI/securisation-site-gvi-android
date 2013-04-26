9bcc898c1d1be0463dbe249a1de1b5f214d0d6c3
damienChesneau <ches.damien@gmail.com>
2013-04-26 12:52:55 +0200

    update link

--- src/fr/securisation_site_gvi/client/AccueilActivity.java
+++ src/fr/securisation_site_gvi/client/AccueilActivity.java
@@ -87,7 +87,7 @@ public class AccueilActivity extends TemplateActivity {
         });
         this.gererLesAuthorisationAcces.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(activityContext, AuthorisationAcces.class);
+                Intent intent = new Intent(activityContext, GererAuthorisationAcces.class);
                 startActivity(intent);
             }
         });
